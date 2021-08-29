0db193161165e6846710c9793fc3368f9011a79e
Seydou Toloba <seydou.toloba.st@gmail.com>
2021-08-29 16:22:33 +0000

    teste2908

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+export const firebaseConfig = {
+  apiKey: 'AIzaSyC7YNZW6QOK9Kq5UR_MzafSjFPQQCvZxIc',
+  authDomain: 'fms2021-fd5e0.firebaseapp.com',
+  projectId: 'fms2021-fd5e0',
+  storageBucket: 'fms2021-fd5e0.appspot.com',
+  messagingSenderId: '961618366659',
+  appId: '1:961618366659:web:cdca87244e29ef81459276'
+};
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
